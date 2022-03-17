bd32a5fd3c821a21c5a66ccbe7fc717ecc82c336
Armeet Jatyani <armeetjatyani@gmail.com>
2022-03-17 11:58:54 -0700

    update March 17th

--- src/data/agendas.js
+++ src/data/agendas.js
@@ -1,11 +1,30 @@
 const agendas = [
-  {
-    date: "March 17th, 2022",
-    summary: "Elections kickoff! Mandatory for those who'd like to run. Codequest kickoff! Explaining process, date, and teams!",
-    link: "https://docs.google.com/presentation/d/1d1Z5t6lBALMBEKm9tLMH28oCMuYrgrWH0Cb_OseWEcg/edit?usp=sharing",
-    attendance: 30,
-  },
-  
-]
+	{
+		date: "March 17th, 2022",
+		summary: "New website. Elections kickoff! Mandatory for those who'd like to run. Codequest kickoff! Explaining process, date, and teams!",
+		link: "https://docs.google.com/presentation/d/1d1Z5t6lBALMBEKm9tLMH28oCMuYrgrWH0Cb_OseWEcg/edit?usp=sharing",
+		extraLinks: [
+			{
+				name: "Attendance",
+				path: "https://tinyurl.com/attendevpc",
+			},
+			{
+				name: "New website!",
+				path: "http://evpc.club",
+			},
+			{
+				name: "Slideshow",
+				path: "https://docs.google.com/presentation/d/1d1Z5t6lBALMBEKm9tLMH28oCMuYrgrWH0Cb_OseWEcg/edit?usp=sharing",
+			},
+		],
+	},
+	{
+		date: "March 3rd, 2022",
+		summary: "Programming Jeopardy! 2 teams compete for the most points.",
+		link: "#",
+		extraLinks: [{ name: "Jeopardy game", path: "https://jeopardylabs.com/play/coding-jeopardy39" }],
+		attendance: 16,
+	},
+];
 
 export default agendas;
--- src/pages/agendas.js
+++ src/pages/agendas.js
@@ -9,7 +9,7 @@ const Agendas = () => {
 				<h1 className="text-3xl font-black">Agendas</h1>
 				{agendas.map((agenda) => {
 					return (
-						<a key={agenda.date} href={agenda.link} target="_blank" rel="noreferrer" className="block p-6 space-y-4 transition-all duration-300 rounded-2xl hover:bg-gray-800 hover:scale-[1.02] hover:shadow-lg ring-purple-300 ring-1">
+						<a key={agenda.date} href={agenda.link} target="_blank" rel="noreferrer" className="block p-6 space-y-4 transition-all duration-300 rounded-2xl hover:bg-gray-800 hover:shadow-lg ring-purple-300 ring-1">
 							<div className="flex items-center space-x-4">
 								<h2 className="text-lg font-black underline">{agenda.date}</h2>
 								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -19,12 +19,29 @@ const Agendas = () => {
 								</svg>
 							</div>
 							<p>{agenda.summary}</p>
-							<div className="flex items-center justify-center px-4 space-x-2 text-gray-800 bg-purple-300 rounded-lg w-fit">
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-									<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-									<circle cx="12" cy="7" r="4"></circle>
-								</svg>
-								<p>{agenda.attendance}</p>
+							{agenda.attendance && (
+								<div className="flex items-center justify-center px-4 space-x-2 text-gray-800 bg-purple-300 rounded-lg w-fit">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+										<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+										<circle cx="12" cy="7" r="4"></circle>
+									</svg>
+									<p>{agenda.attendance}</p>
+								</div>
+							)}
+
+							<div className="flex space-x-2">
+								{agenda.extraLinks.map((link) => {
+									return (
+										<a href={link.path} key={link.name} target="_blank" className="w-fit px-4 flex items-center justify-center space-x-2 bg-purple-300 text-gray-800 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+												<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+												<polyline points="15 3 21 3 21 9"></polyline>
+												<line x1="10" y1="14" x2="21" y2="3"></line>
+											</svg>
+											<p className="">{link.name}</p>
+										</a>
+									);
+								})}
 							</div>
 						</a>
 					);
