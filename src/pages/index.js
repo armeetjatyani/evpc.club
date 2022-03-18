a47e55046245447c5cc4dea90b73da4fa1cc37e2
Armeet Jatyani <armeetjatyani@gmail.com>
2022-03-18 01:07:29 -0700

    added hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import Image from "next/image";
 
 export default function Home() {
 	return (
@@ -6,16 +7,19 @@ export default function Home() {
 			<Header />
 
 			{/* Main Content */}
-			<main className="w-[70%] h-[700px] md:h-[550px] flex flex-col space-y-4 items-center justify-center">
+			<main className="w-[70%] md:h-[550px] flex flex-col space-y-4 items-center justify-center">
 				{/* Terminal */}
-				<svg xmlns="http://www.w3.org/2000/svg" width={200} height={200} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+				<svg xmlns="http://www.w3.org/2000/svg" width={100} height={100} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
 					<polyline points="4 17 10 11 4 5"></polyline>
 					<line x1="12" y1="19" x2="20" y2="19"></line>
 				</svg>
-				<h1 className="font-sans text-5xl font-black text-center">
+				<h1 className="font-sans text-3xl font-black text-center">
 					Evergreen Valley High School <br></br>Programming Club
 				</h1>
-				<p className="w-[70%] text-center text-xl">The party club at EV 🥳 🎉 - Lunch Meetings @ B138</p>
+				<p className="w-[70%] text-center">The party club at EV 🥳 🎉 - Lunch Meetings @ B138</p>
+				<div className="relative w-full transition-all duration-500 rounded-xl h-[400px] lg:h-[1000px] shadow-2xl overflow-clip hover:scale-105">
+					<Image src={"/hero.jpg"} layout="fill" objectFit="cover" priority className="blur-[2px] brightness-125 transition-all duration-500" />
+				</div>
 			</main>
 		</div>
 	);
